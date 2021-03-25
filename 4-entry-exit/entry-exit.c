dda4bb7f1bf5e4d87844ab8b6753fcc22cfd4b08
purplewall1206 <wangzc5514@163.com>
2021-03-25 19:53:57 +0800

    entry exit

--- 4-entry-exit/entry-exit.c
+++ 4-entry-exit/entry-exit.c
@@ -1,27 +1,5 @@
-#include <stdio.h>
+#include "header.h"
 
-#define DUMP(func, call) \
-	printf("%s: func = %p, called by = %p\n", __FUNCTION__, func, call)
- 
- 
-void __attribute__((no_instrument_function))
-__cyg_profile_func_enter(void *this_func, void *call_site)
-{
-	DUMP(this_func, call_site);
-}
- 
- 
-void __attribute__((no_instrument_function))
-__cyg_profile_func_exit(void *this_func, void *call_site)
-{
-	DUMP(this_func, call_site);
-    // printf("\n");
-}
-
-int a(char *s) {
-    printf("func a trigger: %s\n", s);
-    return 0;
-}
 
 int b(int c) {
     char *x = "entry exit test";
--- 4-entry-exit/script.sh
+++ 4-entry-exit/script.sh
@@ -4,3 +4,9 @@ clang -finstrument-functions  -o ee-llvm entry-exit.c
 
 ./ee-gcc
 ./ee-llvm
+
+
+gcc -finstrument-functions entry-exit.c header.h a.c -o ee-gcc
+clang -finstrument-functions entry-exit.c header.h a.c 
+# clang -finstrument-functions entry-exit.c header.h a.c -o ee-llvm
+# clang-12: error: cannot specify -o when generating multiple output files
