44d85a3a5a4c46a1d91905045b840c79eb2568de
purplewall1206 <wangzc5514@163.com>
2021-03-26 20:59:24 +0800

    sync

--- tutorial/5-pass-test/script.sh
+++ tutorial/5-pass-test/script.sh
@@ -1,1 +1,3 @@
 opt --disable-output -load ~/Documents/llvm-project-master/llvm-build/lib/LLVMHello.so -hello base.bc 
+opt --disable-output -load ~/Documents/llvm-project-master/llvm-build/lib/LLVMHello.so -hello -time-passes base.bc 
+
