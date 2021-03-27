e38991673e83883d590d0cb42383e574e3404ebd
purplewall1206 <wangzc5514@163.com>
2021-03-27 20:33:16 +0800

    pass compile with clang commdline

--- tutorial/5-pass-test/script.sh
+++ tutorial/5-pass-test/script.sh
@@ -1,3 +1,13 @@
 opt --disable-output -load ~/Documents/llvm-project-master/llvm-build/lib/LLVMHello.so -hello base.bc 
 opt --disable-output -load ~/Documents/llvm-project-master/llvm-build/lib/LLVMHello.so -hello -time-passes base.bc 
 
+opt -load ~/Documents/llvm-project-master/llvm-build/lib/LLVMHello.so -gvn -licm --disable-output --debug-pass=Structure base.bc 
+
+opt -load ~/Documents/llvm-project-master/llvm-build/lib/LLVMHello.so -gvn -hello -licm --disable-output --debug-pass=Structure base.bc 
+
+gdb opt
+    break llvm::PassManager::run
+    run base.bc -load ~/Documents/llvm-project-master/llvm-build/lib/LLVMHello.so -hello
+
+
+clang `llvm-config --cxxflags` -Wl,-znodelete -fno-rtti -fPIC -shared Hello.cpp -o LLVMHello.so `llvm-config --ldflags`
