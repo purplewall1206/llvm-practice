9a32092a20f4fa76878ec985bf9b0134ecabbd7c
purplewall1206 <wangzc5514@163.com>
2021-04-04 21:07:10 +0800

    sync

--- tutorial/3-clang/script.sh
+++ tutorial/3-clang/script.sh
@@ -12,3 +12,12 @@ clang -S test.c -o -
 clang -O3 -emit-llvm test.c -c -o test.bc
 
 lli test.bc
+
+
+clang -cc1 -analyzer-checker-help
+clang -fsyntax-only test.c 
+clang -cc1 -analyze -analyzer-checker=core test.c 
+clang --analyze -Xanalyzer -analyzer-checker=core test.c
+clang --analyze -Xanalyzer -analyzer-checker=core test.c -o report
+
+clang-check test.c -ast-dump --
