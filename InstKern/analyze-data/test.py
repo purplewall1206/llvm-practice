5b7cb87a251a7df1d7eb29b92a06cfd24206e117
purplewall1206 <wangzc5514@163.com>
2021-04-20 15:43:40 +0800

    static

---
+++ InstKern/analyze-data/test.py
@@ -0,0 +1,56 @@
+# s1 = "balloc.o bitmap.o block_validity.o dir.o ext4_jbd2.o extents.o extents_status.o file.o fsmap.o fsync.o hash.o ialloc.o indirect.o inline.o inode.o ioctl.o mballoc.o migrate.o mmp.o move_extent.o namei.o page-io.o readpage.o resize.o super.o symlink.o sysfs.o xattr.o xattr_hurd.o xattr_trusted.o xattr_user.o fast_commit.o acl.o xattr_security.o  verity.o"
+
+# ll = s1.split(' ')
+# ll.sort()
+# lclang = list()
+# with open('clang.txt', 'r') as f:
+#     lclang = f.readlines()
+
+# lclang.sort()
+# print(len(lclang))
+# print(len(ll))
+# i = 0
+
+# # tmpl = (lclang[0].split(' '))
+# # print(tmpl[-1])
+# for x in ll:
+#     print(x[:-1]+"ll:\t")
+#     for y in lclang:
+#         if y.find(x[:-2]) != -1:
+#             print("\t"+y)
+#             break
+#     print(x[:-2]+"-inst.ll: "+ x[:-1]+"ll\t" +   "\n\topt -load ./LLVMinstkern.so -analyze -inst fs/ext4/"+x[:-1]+"ll  -o  fs/ext4/"+ x[:-2]+"-inst.ll")
+#     print(x+": " + x[:-2] + "-inst.ll  \t\n\tllc -code-model=kernel --filetype=obj fs/ext4/"+x[:-1] + "ll  \n")
+
+# print("all: " + s1)
+
+allinst = 0
+store = 0
+icall = 0
+ret = 0
+# instructions : 379
+# store instructions: 17
+# icall instructions: 0
+# ret   instructions: 4
+with open('compile.log', 'r') as f:
+    for line in f.readlines():
+        if line.find('instructions :')!=  -1:
+            # print(line)
+            allinst = allinst + int(line.split(":")[-1])
+        elif line.find('store instructions:') != -1:
+            store = store + int(line.split(':')[-1])
+        elif line.find('icall instructions:') != -1:
+            icall = icall + int(line.split(':')[-1])
+        elif line.find('ret   instructions:') != -1:
+            ret = ret + int(line.split(':')[-1])
+
+print(allinst)
+print(store)
+print(icall)
+print(ret)
+
+print(float(store/allinst))
+print(float(icall/allinst))
+print(float(ret/allinst))
+
+
---
+++ InstKern/statistic.cpp
@@ -0,0 +1,74 @@
+#include "llvm/ADT/MapVector.h"
+#include "llvm/IR/AbstractCallSite.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/InlineAsm.h"
+
+#include <vector>
+
+using namespace llvm;
+
+namespace {
+    struct StatisticModulePass : public ModulePass {
+        static char ID;
+        StatisticModulePass() : ModulePass(ID) {}
+        bool runOnModule(Module &M) override;
+        void print(raw_ostream &OutS, const Module *M) const override ;
+        std::vector<Instruction*> storeVec;
+        std::vector<Instruction*> icallVec;
+        std::vector<Instruction*> retVec;
+    };
+
+    bool StatisticModulePass::runOnModule(Module &M)  {
+        auto& CTX = M.getContext();
+        outs() << "instructions : " << M.getInstructionCount() << "\n";
+        for (auto& F : M) {
+            for (auto& BB : F) {
+                for (auto& Ins : BB) {
+                    if (Ins.getOpcode() == Instruction::Store) {
+                        storeVec.push_back(&Ins);
+                    }
+                    if (Ins.getOpcode() == Instruction::Call) {
+                        auto *CB = dyn_cast<CallBase>(&Ins);
+                        if (CB->isIndirectCall()) {
+                            icallVec.push_back(&Ins);
+                        }
+                    }
+                    if (Ins.getOpcode() == Instruction::Ret) {
+                        retVec.push_back(&Ins);
+                    }
+
+                }
+            }
+        }
+
+        outs() << "store instructions: " << storeVec.size() << "\n";
+        outs() << "icall instructions: " << icallVec.size() << "\n";
+        outs() << "ret   instructions: " << retVec.size() << "\n"; 
+        return true;
+    }
+
+    void StatisticModulePass::print(raw_ostream &O, const Module *M) const {
+        O << "========statistic output========\n";
+        // 如果有-o那么这个函数则不会输出
+        O << "store instructions: " << storeVec.size() << "\n";
+        // for (auto& Ins : storeVec) {
+        //     O << "    " << Ins->getFunction()->getName() << ":" << *Ins << "\n";
+        // }
+        O << "icall instructions: " << icallVec.size() << "\n";
+        O << "ret   instructions: " << retVec.size() << "\n"; 
+    }
+
+    char StatisticModulePass::ID = 0;
+    RegisterPass<StatisticModulePass> 
+        X (/*PassArg=*/"inst",
+        /*Name=*/"StatisticModulePass",
+        /*CFGOnly=*/true,
+        /*is_analysis=*/true);
+}
