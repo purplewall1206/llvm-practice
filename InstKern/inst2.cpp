59f3fcd5bb6e165faa951682998bb4070abde4f1
purplewall1206 <wangzc5514@163.com>
2021-04-26 11:09:55 +0800

    instrument cal_tag

--- InstKern/inst2.cpp
+++ InstKern/inst2.cpp
@@ -43,15 +43,17 @@ namespace {
         void generateStoreVec(Module &M);
         // bool declareFunction(Module &M, StringRef name);
         bool isException(StringRef name);
-        Value* inst_cal_tag(Value* vaddr, IRBuilder<> &builder);
-        Value* inst_get_rbp(IRBuilder<> &builder);
-        void inst_check_tag(Value* vaddr, IRBuilder<> &builder);
+        Value* inst_cal_tag(Value* vaddr, IRBuilder<> &builder, Module &M);
+        Value* inst_get_rbp(IRBuilder<> &builder, Module &M);
+        void inst_check_tag(Value* vaddr, IRBuilder<> &builder, Module &M);
     };
 
     bool Inst2ModulePass::isException(StringRef name) {
-        if (std::find(exceptVec.begin(), exceptVec.end(), name) == exceptVec.end()) {
+        if (std::find(exceptVec.begin(), exceptVec.end(), name) != exceptVec.end()) {
+            // errs() << "is exception " << name << "\n";
             return true;
         }
+        // errs() << "not exception " << name << "\n";
         return false;
     }
 
@@ -76,8 +78,28 @@ namespace {
     // bool Inst2ModulePass::declareFunction(Module &M, StringRef name) {
 
     // }
-    Value* Inst2ModulePass::inst_cal_tag(Value *v, IRBuilder<> &builder) {
-        
+    Value* Inst2ModulePass::inst_cal_tag(Value *vaddr, IRBuilder<> &builder, Module &M) {
+// define dso_local i64 @cal_tag(i64 %0) #0 {
+//   %2 = alloca i64, align 8
+//   store i64 %0, i64* %2, align 8
+//   %3 = load i64, i64* %2, align 8
+//   %4 = lshr i64 %3, 3
+//   %5 = add i64 %4, -2305904032109035520
+//   ret i64 %5
+// }
+        auto &CTX = M.getContext();
+        Type *intTy64 = Type::getInt64Ty(CTX);
+        // outs() << "intty64\n";
+        AllocaInst *AI = builder.CreateAlloca(intTy64, nullptr, "cal_tag_alloca2");
+        Value *ST = builder.CreateStore(vaddr, AI, false);
+        // errs() << "after store\n";
+        LoadInst *LT = builder.CreateLoad(AI, "cal_tag_load3");
+        // errs() << "after load\n";
+        Value *LSHR = builder.CreateLShr(LT, 3, "cal_tag_lshr4", false);
+        // errs() << "after lshr\n";
+        Value *ADD = builder.CreateAdd(LSHR, ConstantInt::get(intTy64, 0xdfffc88000000000), "cal_tag_add5");
+        // errs() << "after add\n";
+        return ADD;
     }
 
     bool Inst2ModulePass::runOnModule(Module &M)  {
@@ -85,7 +107,14 @@ namespace {
         outs() << "instructions : " << M.getInstructionCount() << "\n";
         outs() << "store instructions: " << storeVec.size() << "\n";
 
-        
+        generateStoreVec(M);
+        Instruction* Ins = storeVec[0];
+        std::vector<Value*> param;
+        for (Use &U : Ins->operands()) {
+            param.push_back(U.get());
+        }
+        IRBuilder<> builder(Ins);
+        inst_cal_tag(param[0], builder, M);
         return true;
     }
 
