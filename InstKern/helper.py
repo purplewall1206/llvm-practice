5f5a59db91ac4f97381d7f4f19c09f932b70ee29
purplewall1206 <wangzc5514@163.com>
2021-05-03 11:28:35 +0800

    sync

--- InstKern/helper.py
+++ InstKern/helper.py
@@ -5,11 +5,15 @@ x = 0xffffb9902b76b950
 y = 0xffff888100000000
 z = 0xffff888220000000
 
+a = 0xffffb9900fb0aa03
+
 def recal(x):
     x = (x-ketag_offset) << 3
     return x
 
 print(hex(recal(x)))
+print(hex(recal(a)))
+
 
 def cal(x):
     x = (x >> 3) + ketag_offset
